25bd33dcf56e893302e98546bc260bfef32e36fc
Fábry Kevin 9.C <90604112+fabrykevin@users.noreply.github.com>
2022-11-16 11:21:33 +0100

    menu

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 from menu import *
+from fuggvenyek import showall, readFile, search
+
+readFile()
+choice=-1
+while choice != 0:
+    choice=menu()
+    if choice == 1:
+        showall()
+    elif choice == 2:
+        cipok=input('Kérem a cipőt')
+        result=search(cipok)
+        if len(result)==0:
+            print('Nincs ilyen cipő.')
+        else:
+            for item in result:
+                print(f'\t{item}')
+    elif choice == 3:
+        legolcsobb(), legdrágább() 
+    elif choice == 4:
+        Legújabb() , legrégebbi()
