033297e2dad6288439c4191437ef55b6d052da09
Hegedüs Márk <hegedus.mark@students.jedlik.eu>
2022-11-16 11:29:39 +0100

    fuggvegyek1

--- fuggvenyek.py
+++ fuggvenyek.py
@@ -1,8 +1,22 @@
 from Nevek import nevek
 
+data = []
+
 def registration(startNumber):
     for nevek in nevek:
         nameList = nevek.split(';')
         if nameList[0] == startNumber:
             return nameList[6]
     return False
+
+def readfile():
+    file = open('Sneaker.csv', 'r', encoding='utf-8')
+
+    for row in file:
+        data.append(row.strip())
+
+    file.close()
+
+def searchBrand():
+    file = open('Sneaker.csv', 'r', encoding='utf-8')
+    splittedData = row.split(';')
