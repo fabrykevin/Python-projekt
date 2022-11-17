9651228e21e78a96295ac72a975860ff49aa88a2
Fábry Kevin 9.C <90604112+fabrykevin@users.noreply.github.com>
2022-11-17 08:14:42 +0100

    .

--- fuggvenyek.py
+++ fuggvenyek.py
@@ -7,14 +7,6 @@ def registration(startNumber):
             return nameList[6]
     return False
 
-<<<<<<< HEAD
 
 
-def    bejelentkezes
 
-
-=======
-def searchBrand(márka):
-    file = open('Sneaker.csv', 'r', encoding='utf-8')
-    splittedData = file
->>>>>>> 02686cf3c457afd19bc24d33a7f4d8ed57ecc408
