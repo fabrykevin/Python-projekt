cad450636fefb608348bd7a907ac2e3801a34ec3
Fábry Kevin 9.C <90604112+fabrykevin@users.noreply.github.com>
2022-11-23 13:25:14 +0100

    nem jó

--- bejelentkezés.py
+++
@@ -1,9 +0,0 @@
-data=[]
-def readFile():
-    file =  open('nevek.csv','r', encoding='utf-8')
-    firstRow=file.readline()
-    for row in file: 
-      
-        data.append(row.strip())
-    file.close()
-
--- menu.py
+++ menu.py
@@ -3,12 +3,12 @@ def menu():
     option=-1
     while option< 0 or option>5:
         print('---------------------')
-        print('0 - Regisztráció') 
-        print('1 - Az összes cipő megmutatása')
-        print('2 - A Kedvenc cipő megkeresése ')
-        print('3 - A legdrágább és a legolcsóbb cipők megmutatása ')
-        print('4 - Legújabb és a legrégebbi cipők megmutatása ')
-        print('5 -  Kilépés')
+        print('1 - Regisztráció') 
+        print('2 - Az összes cipő megmutatása')
+        print('3 - A Kedvenc cipő megkeresése ')
+        print('4 - A legdrágább és a legolcsóbb cipők megmutatása ')
+        print('5 - Legújabb és a legrégebbi cipők megmutatása ')
+        print('6 -  Kilépés')
         print('---------------------')
         option=int(input('Válasszon a lehetőségek közül :'))
     return option
