5e92774483e398d1a34d2147d096830730be777a
Hegedüs Márk <hegedus.mark@students.jedlik.eu>
2022-11-24 09:24:18 +0100

    upper

--- fuggvenyek.py
+++ fuggvenyek.py
@@ -26,8 +26,8 @@ def readFile():
 def searchSizeByType():
     file = open('sneaker.csv', 'r', encoding='utf-8')
     file.readline()
-    Brand = input("Adja meg a cipő márkáját: ")
-    Type = input("Add meg a cipő típusát: ")
+    Brand = input("Adja meg a cipő márkáját: ").upper()
+    Type = input("Add meg a cipő típusát: ").upper()
     for row in file:
         splitted = row.split(';')
         if splitted[1] == Brand and splitted[2] == Type:
