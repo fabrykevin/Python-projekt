4325d8bd8855129036a6fb9985861d837ae607b1
Kovács Máté Miklós (9.c) <kovacs.mate.miklos@students.jedlik.eu>
2022-11-28 19:01:45 +0100

    commit

--- menu.py
+++ menu.py
@@ -5,7 +5,7 @@ def menu():
         print('---------------------')
         print(' 1 - Regisztráció/ Belépés') 
         print(' 2 - A teljes kínálat megmutatása')
-        print(' 3 - A Kedvenc cipő megkeresése ')
+        print(' 3 - A Megfelelő cipő megkeresése ')
         print(' 4 - A legdrágább és a legolcsóbb cipők megmutatása ')
         print(' 5 - Legújabb és a legrégebbi cipők megmutatása ')
         print(' 6 - Kilépés')
