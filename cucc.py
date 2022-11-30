cbed6078bf74081480118b326848d7485ad51a2a
kovacs mate <kovacs.mate.miklos@students.jedlik.eu>
2022-11-30 11:44:41 +0100

    .

--- cucc.py
+++ cucc.py
@@ -2,7 +2,6 @@ from menu import *
 from main import *
 from fuggvenyek import *
 from nevek import *
-from fileHandling import *
 
 sneaker = []
 
@@ -30,24 +29,3 @@ def smallest():
             minSize = int(sneakerData[4])
             minName = sneakerData[1]
 
-def lowest():
-    for x in shoes:
-        lowest = 99999999999999999
-        shoe = ""
-        splittedData = x.split(";")
-        if splittedData{6} < int(lowest):
-            shoe = splittedData{0}
-            lowest = splittedData{6}
-    return lowest
-        
-
-
-def highest():
-    for x in shoes:
-        highest = 0
-        shoe =""
-        splittedData = x.split(";")
-        if splittedData{6} > int(highest):
-            shoe = splittedData{0}
-            highest =splittedData{6}
-    return highest
